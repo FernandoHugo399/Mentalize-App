b84250c7c0699c18a76f0aaf8416f6d0219f0d5d
Hugo Fernando <huginho888@outlook.com>
2022-08-24 16:57:20 -0300

    Finalizando splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonRouterOutlet, Platform } from '@ionic/angular';
 import { App } from '@capacitor/app';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,18 @@ import { App } from '@capacitor/app';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    this.showSplash();
 
+  }
+
+  private async showSplash() {
+    await SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+      fadeInDuration: 100,
+      fadeOutDuration: 100
+    });
+  }
 }
 
