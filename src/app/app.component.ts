8942ec5df4a2ae49946c493daacff3e52ada8f36
Hugo Fernando <huginho888@outlook.com>
2022-08-24 21:03:00 -0300

    Otimizando arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { Router, NavigationEnd } from '@angular/router';
 import { App } from '@capacitor/app';
+import { AlertController, Platform } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
 import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
@@ -9,18 +12,65 @@ import { SplashScreen } from '@capacitor/splash-screen';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-    this.showSplash();
-
+  private routesUrl = [];
+  constructor(
+    private platform: Platform,
+    private router: Router,
+    private alertController: AlertController,
+    private location: Location,
+  )
+  {
+    this.platform.ready().then(()=>{
+      SplashScreen.hide();
+      this.routerEvent();
+      this.backButtonEvent();
+    });
   }
 
-  private async showSplash() {
-    await SplashScreen.show({
-      showDuration: 1000,
-      autoHide: true,
-      fadeInDuration: 100,
-      fadeOutDuration: 100
+  private routerEvent(){
+    this.router.events.pipe( filter( ( event: NavigationEnd ) => ( event instanceof NavigationEnd )))
+    .subscribe((event: NavigationEnd)=>{
+      if(this.routesUrl.length === 0) {
+        this.routesUrl.push(event.id, event.id);
+      } else {
+        this.routesUrl.push(event.id);
+      }
     });
+  }
+
+  private backButtonEvent() {
+    this.platform.backButton.subscribeWithPriority(999999, ()=>{
+      const url = this.router.routerState.snapshot.url;
+      this.routesUrl.splice(-2);
+
+      if(url === '/tabs/home'){
+        if(this.routesUrl.length === 0) {
+          this.backButtonALert();
+        }
+      }
+
+      this.location.back();
+    });
+  }
+
+  private async backButtonALert() {
+    const alert = await this.alertController.create({
+      message: 'Você quer fechar o App ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'Cancel'
+        },
+        {
+          text: 'Fechar App',
+          handler: () => {
+            App.exitApp();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
 
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,10 +1,4 @@
-import { Location } from '@angular/common';
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { App } from '@capacitor/app';
-import { AlertController, IonTabs, Platform } from '@ionic/angular';
-import { filter } from 'rxjs/operators';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
@@ -12,65 +6,5 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  private routesUrl = [];
-  constructor(
-    private platform: Platform,
-    private router: Router,
-    private alertController: AlertController,
-    private location: Location,
-  )
-  {
-    this.routerEvent();
-
-    this.platform.ready().then(()=>{
-      this.backButtonEvent();
-    });
-  }
-
-  private routerEvent(){
-    this.router.events.pipe( filter( ( event: NavigationEnd ) => ( event instanceof NavigationEnd )))
-    .subscribe((event: NavigationEnd)=>{
-      if(this.routesUrl.length === 0) {
-        this.routesUrl.push(event.id, event.id);
-      } else {
-        this.routesUrl.push(event.id);
-      }
-    });
-  }
-
-  private backButtonEvent() {
-    this.platform.backButton.subscribeWithPriority(999999, ()=>{
-      const url = this.router.routerState.snapshot.url;
-      this.routesUrl.splice(-2);
-
-      if(url === '/tabs/home'){
-        if(this.routesUrl.length === 0) {
-          this.backButtonALert();
-        }
-      }
-
-      this.location.back();
-    });
-  }
-
-  private async backButtonALert() {
-    const alert = await this.alertController.create({
-      message: 'Você quer fechar o App ?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'Cancel'
-        },
-        {
-          text: 'Fechar App',
-          handler: () => {
-            App.exitApp();
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
 
 }
