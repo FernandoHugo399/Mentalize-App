8c263877e9d339f3a419a4d2060006858f87a551
sameckmatheus <sameckmatheuspro@gmail.com>
2022-09-11 12:43:01 -0300

    Add Swiper Slide Options

--- src/app/components/slides/slides.page.ts
+++ src/app/components/slides/slides.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidesPage implements OnInit {
 
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
   constructor() { }
 
   ngOnInit() {
