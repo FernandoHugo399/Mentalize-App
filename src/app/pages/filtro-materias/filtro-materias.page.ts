9bd32baa8208a8bdbe6f2b09da12ca99b58b8eeb
Hugo Fernando <huginho888@outlook.com>
2022-09-15 15:47:27 -0300

    Finalizando página filtro por matéria

--- src/app/pages/filtro-materias/filtro-materias.page.ts
+++ src/app/pages/filtro-materias/filtro-materias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Publish } from 'src/app/interfaces/publish';
 import { PublishService } from 'src/app/services/publish.service';
 
@@ -9,15 +10,17 @@ import { PublishService } from 'src/app/services/publish.service';
 })
 export class FiltroMateriasPage implements OnInit {
   public publish: Publish[];
+  public matter: string;
 
-  constructor(private publishService: PublishService) { }
+  constructor(private publishService: PublishService, private activatedRoute: ActivatedRoute) { }
 
   async ngOnInit() {
+    this.matter = this.activatedRoute.snapshot.paramMap.get('materia') as string;
     this.publish = await this.getPublish();
   }
 
   async getPublish() {
-    return await this.publishService.getPublishs();
+    return await this.publishService.getPublishByMatter(this.matter);
   }
 
   async doRefresh(event: any) {
