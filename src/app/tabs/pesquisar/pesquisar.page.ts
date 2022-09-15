8451e13d88bea9ecc077e29c043882151ae0a8a6
sameckmatheus <sameckmatheuspro@gmail.com>
2022-09-15 20:03:40 -0300

    Update Search Bar

--- src/app/tabs/pesquisar/pesquisar.page.ts
+++ src/app/tabs/pesquisar/pesquisar.page.ts
@@ -1,6 +1,8 @@
+import { Publish } from './../../interfaces/publish';
 import { DataService, INivelEnsino } from './../../services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { IMateria } from 'src/app/services/data.service';
+import { SearchbarCustomEvent, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pesquisar',
@@ -10,6 +12,21 @@ import { IMateria } from 'src/app/services/data.service';
 export class PesquisarPage implements OnInit {
   public nivelEnsino: INivelEnsino[];
   public materias: IMateria[];
+  public allPublishes: Publish[];
+  public queryText = '';
+  public publishes: Publish[];
+
+  public filterInstitutions(_event: Event){
+    const event = _event as SearchbarCustomEvent;
+    const query = event.target.value;
+
+    if(query && query.trim() !== ''){
+      this.publishes = this.allPublishes
+      .filter((publishes)=> publishes.titulo.toLowerCase().indexOf(query.toLowerCase()) > -1 );
+    } else {
+      this.publishes = this.allPublishes;
+    }
+  }
 
   constructor(private dataService: DataService) {
     this.nivelEnsino = dataService.nivelEnsino;
