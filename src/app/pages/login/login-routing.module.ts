7536162ad2e34ed7d7c28e11cf00cf4e33ca86d5
Matheus Sameck <105814501+sameckmatheus@users.noreply.github.com>
2022-09-16 13:38:19 -0300

    Update login-routing.module.ts

--- src/app/pages/login/login-routing.module.ts
+++ src/app/pages/login/login-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginPage } from './login.page';
+import { SlidesPage } from './slides.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginPage
+    component: SlidesPage
   }
 ];
 
