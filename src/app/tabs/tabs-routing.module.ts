e7c0df98b36c8f600a26883a609e2b6142a498cb
Matheus Sameck <105814501+sameckmatheus@users.noreply.github.com>
2022-09-16 13:23:38 -0300

    Update Slides Routes

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -25,14 +25,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/home',
+        redirectTo: '/components/slides',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/home',
+    redirectTo: '/components/slides',
     pathMatch: 'full'
   }
 ];
